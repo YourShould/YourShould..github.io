ea6b9431446e945d71a60d0c4e7cf5fb64358ef9
Mr.D <812313299@qq.com>
2024-05-14 23:01:42 +0800

    Add files via upload

---
+++ assets/detail.2c048776.js
@@ -0,0 +1,1 @@
+import{e}from"./index.fdf04471.js";const o=e("detail",{state:()=>({item:JSON.parse(window.sessionStorage.getItem("item")||"{}")})});export{o as u};
---
+++ assets/setting.aa29e6ad.js
@@ -0,0 +1,1 @@
+import{e}from"./index.fdf04471.js";const o=e("setting",{state:()=>({domain:window.localStorage.getItem("domain")||"",isSmallScreen:!0,isAnimation:!1})});export{o as u};
